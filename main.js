526ad180ea3354c25f3ee0e46691e96caf34a38f
Yago <yago.pereira@estudante.ufjf.br>
2023-03-19 16:21:59 -0300

    Projeto palíndromo finalizado.

--- main.js
+++ main.js
@@ -2,19 +2,30 @@ const formulario = document.getElementById('formulario');
 const texto = document.getElementById('palavra');
 const resultado = document.getElementById('resultado');
 
+formulario.addEventListener('submit', acaoNoFormulario);
+
 function acaoNoFormulario(e) {
     e.preventDefault();
     const palavra = texto.value;
-    const converte = palavra.toLowerCase();
-    const separarPalavra = converte.split('');
+    const palavraLower = palavra.toLowerCase();
+    const separarPalavra = palavraLower.split('');
     const inverterPalavra = separarPalavra.reverse('');
     const palavraInvertida = inverterPalavra.join('');
-    if (converte == palavraInvertida) {
-        resultado.innerHTML = `<p>A palavra ${palavra} é um palíndromo.</p>`;
+
+    resultado.innerHTML = palavra == '' ? '<p>Por favor, digite uma palavra.</p>' : verificaSeEPalindromo(palavra, palavraLower, palavraInvertida);
+}
+
+function verificaSeEPalindromo (palavra, palavraLower, palavraInvertida) {
+    if (palavraLower == palavraInvertida) {
+        return `<p>A palavra ${palavra} é um palíndromo.</p>`;
     }
     else {
-        resultado.innerHTML = `<p>A palavra ${palavra} não é um palíndromo.`
+        return `<p>A palavra ${palavra} não é um palíndromo.`;
     }
 }
 
-formulario.addEventListener('submit', acaoNoFormulario);
+// Cuidando do placeholder
+
+texto.addEventListener('focus', function () { this.placeholder = '' });
+
+texto.addEventListener('blur', function () { this.placeholder = 'Digite uma palavra...' });
